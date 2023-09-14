ef8bcdd3000ebaed06036a7438b0e390c8c32c0e
yavuzhan <gtacinq45@gmail.com>
2023-09-14 18:04:36 +0200

    first deposit

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new typed(".text" ,{
+    String:["Integrateur web" , "Frontend Developpeur" , "Web Developpeur"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
